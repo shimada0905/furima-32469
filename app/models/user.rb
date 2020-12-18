6ad9719fcf3990cc8ffd416f1692166343d017fa
shimada0905 <74903076+shimada0905@users.noreply.github.com>
2020-12-18 16:32:26 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,13 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
-         validates :birth_date, presence: true
+         with_options presence: true do
+          validates :nickname
+          validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/ }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :birth_date
+         end
 end
