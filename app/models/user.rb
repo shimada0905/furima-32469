b705b957c90a2c8d484b5afd747edc029846cf3a
shimada0905 <74903076+shimada0905@users.noreply.github.com>
2020-12-18 21:59:14 +0900

    バリデーションを修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,11 @@ class User < ApplicationRecord
 
          with_options presence: true do
           validates :nickname
-          validates :encrypted_password, format: { with: /\A[a-zA-Z0-9]+\z/ }
           validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
           validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
           validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
           validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
           validates :birth_date
          end
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i }
 end
