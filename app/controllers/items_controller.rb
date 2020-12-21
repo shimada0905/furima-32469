968acfcc3c5adfd54dc37e8895c3d5282db276ba
shimada0905 <74903076+shimada0905@users.noreply.github.com>
2020-12-21 18:00:08 +0900

    ストロングパラメーターを設定し、アクションの処理を記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,10 +3,20 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+    @item = Item.new
   end
 
   def create
-    
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:item_name, :introduction, :category_id, :condition_id, :prefecture_id, :shipping_cost_id, :shipping_day_id, :price, :image).merge(user_id: current_user.id)
   end
 end
