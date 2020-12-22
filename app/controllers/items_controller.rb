0be65b98a89bc43207f7419c720de248625b4a7c
shimada0905 <74903076+shimada0905@users.noreply.github.com>
2020-12-22 19:59:23 +0900

    URLの直接入力を禁止

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.includes(:user)
@@ -19,11 +21,11 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
+
   end
 
   def edit
-    @item = Item.find(params[:id])
+
   end
 
   def update
@@ -40,5 +42,15 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:item_name, :introduction, :category_id, :condition_id, :prefecture_id, :shipping_cost_id,
                                  :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
