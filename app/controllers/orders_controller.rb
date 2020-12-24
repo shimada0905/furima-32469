b934738e199ab5d1fd4761e92f156bf7960edaf4
shimada0905 <74903076+shimada0905@users.noreply.github.com>
2020-12-24 11:06:16 +0900

    コントローラーを作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @order_address = OrderAddress.new(order_params)
+    if @order_address.valid?
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+
+  def order_params
+    params.require(:order_address).permit(:post_code, :prefecture_id, :city, :house_number,:building_name, :tel).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
